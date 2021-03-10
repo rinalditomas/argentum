0f150ff501bb57edec7b058987e07616ea466bbd
FlorPaez-15 <florepaez15@gmail.com>
2021-03-10 13:58:17 -0300

    faltan rutas front

---
+++ frontend/src/container/main.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+import Navbar from "./Navbar";
+import Footer from "../components/Footer";
+
+//ACA SE IMPORTAN LOS COMPONENTES
+
+export default () => {
+  return (
+    <div>
+      <div>
+        <Navbar />
+      </div>
+      <div>
+      
+      
+      </div>
+      <div>
+        <Footer />
+      </div>
+    </div>
+  );
+};
