4ea3d07ccf751484938f14f08c74b28345b9de65
rinalditomas <rinalditomas@gmail.com>
2021-03-11 14:49:32 +0100

    hola

--- frontend/src/state/searchProduct.js
+++ frontend/src/state/searchProduct.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 import { createReducer, createAsyncThunk } from "@reduxjs/toolkit";
 
 export const getSearchProduct = createAsyncThunk('SEARCH_PRODUCT',(search,thunkAPI)=> {
-  return axios.get(``)
+  return axios.get(`/products/${search}`)
    .then(response =>{return(response.data)})
 })
 
