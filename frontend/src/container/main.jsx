f2ef8d009094b8a5d2f3a9d2a8ca2630e0f0f4f9
augustomorales92 <augustomorales92@gmail.com>
2021-03-11 12:26:54 -0300

    cambios

--- frontend/src/container/main.jsx
+++ frontend/src/container/main.jsx
@@ -1,39 +1,21 @@
-
-import React from "react";
-import {Route, Redirect, Switch} from "react-router-dom"
-import SignUp from '../components/signUp'
-import Login from '../components/login'
+import React from 'react'
+import { Switch, Route, Redirect } from "react-router-dom";
 import PrincipalPage from '../components/PrincipalPage'
 import SingleProduct from '../components/SingleProduct'
 import Admin from '../components/Admin'
-import Navbar from '../components/Navbar'
-import Footer from '../components/Footer'
-import SearchProd from '../components/SearchProd'
+
+export default function Main(){
 
 
+    return(
+        <React.Fragment>
 
-
-export default function Main() {
-
-  return (
-      
-      <React.Fragment>
-        {/* <section className="content"> */}
-        <Navbar />
-          <Switch>
-            {/* <div className= 'App'> */}
-            <Route  path="/products" component={PrincipalPage} />
-            <Route exact path= "/signup" component={SignUp} />
-            <Route exact path= "/login" component={Login} />
-            <Route exact path="/search" render={({match})=> <SearchProd match={match}/>} />
-            <Route exact path="/product/:id" render={({match})=> <SingleProduct match={match}/>} />
-            <Route exact path='/admin' component={Admin} />
+            <Switch>
+             <Route  exact path="/products" component={PrincipalPage} />
+             <Route exact path="/productos" render={({match})=> <SingleProduct match={match}/>} />  
+            <Route  exact path="/admin" component={Admin} />
             <Redirect to= "/products" />
-            {/* </div> */}
-          </Switch>
-          <Footer />
-        {/* </section> */}
-      </React.Fragment>
-
-  )
-};
+            </Switch>
+        </React.Fragment>
+    )
+}
